82c19894515361a06b8358ebb884f27b260c2f33
Jānis Z <jzjzjzj.fwd@gmail.com>
2014-12-13 09:30:33 +0200

    General function for injecting scripts by path name.

---
+++ dist/js.js
@@ -0,0 +1,8 @@
+function injectScript(path) {
+  var script = document.createElement('script');
+
+  script.src = chrome.extension.getURL(path);
+
+  (document.head||document.documentElement).appendChild(script);
+  script.parentNode.removeChild(script);
+}
